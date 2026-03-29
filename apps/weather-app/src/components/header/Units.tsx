ecb0ae508f66e84755ffc42d96382b258c25848f
Braian <briantdf4@gmail.com>
2026-03-29 17:54:21 -0300

    Añadir z-index a la clase 'relative' en el componente Units para mejorar la superposición

--- apps/weather-app/src/components/header/Units.tsx
+++ apps/weather-app/src/components/header/Units.tsx
@@ -6,7 +6,7 @@ import { UnitsDropdown } from './UnitsDropdown.tsx'
 export function Units() {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className='relative'>
+    <div className='relative z-10'>
       <button onClick={() => setIsOpen(!isOpen)} className='flex gap-2 px-2.5 py-2 bg-neutral-800 rounded-md md:rounded-lg md:px-4 md:py-3 hover:bg-neutral-700' aria-expanded="false" aria-haspopup="menu">
         <img src={iconUnits} alt="Units" />
         <span className='text-preset-8 md:text-preset-7'>Units</span>
