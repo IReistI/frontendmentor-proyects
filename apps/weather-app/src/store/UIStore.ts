d3f63f0dc723e62bb15a903188126ba835078c44
Braian Nuñez <124116043+IReistI@users.noreply.github.com>
2026-08-03 17:21:34 -0300

    Refactor UIStore interface to SettingsStore
    Updated the interface to SettingsStore with new properties.

--- apps/weather-app/src/store/UIStore.ts
+++ apps/weather-app/src/store/UIStore.ts
@@ -1,10 +1,17 @@
-// interface UIStore {
-//   theme: "light" | "dark"
-//   units: "celsius" | "fahrenheit"
+// interface SettingsStore {
+//   theme: "light" | "dark" | "system"
+//   language: "es" | "en"
+//   unit: "Metrical" | "Imperial"
+//   units: {
+//     temperature: "c" | "f",
+//     wind: "kmh" | "mph",
+//     precepitation": "mm" | "inch"
+//   }
 
-// // favorites: FavoriteLocation[]
+
 
 //   setTheme: () => void
+//   setLanguage: () => void
 //   setUnits: () => void
-//   addFavorite: () => void
+  
 // }
